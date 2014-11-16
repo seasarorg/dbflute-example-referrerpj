6b1a56ccf7048078d1a1ca8d501b1691a8145d33
jflute <dbflute@gmail.com>
2014-11-16 21:59:31 +0900

    (DBFLUTE-1169){Java/C#}: 複合PKの制約上のカラム順序がテーブル上の定義順を違っても差分チェック正しく

---
+++ dbflute-bhvap-spring-example/src/main/java/com/example/dbflute/postgresql/dbflute/bsbhv/BsWhiteCompoundPkWrongOrderBhvAp.java
@@ -0,0 +1,21 @@
+package com.example.dbflute.postgresql.dbflute.bsbhv;
+
+import com.example.dbflute.postgresql.dbflute.exbhv.WhiteCompoundPkWrongOrderBhv;
+
+/**
+ * The base behavior of white_compound_pk_wrong_order as implementation of an application.
+ * <p>
+ * You can NOT implement your original methods here.
+ * This class is overridden when re-generating.
+ * (basically for definitions of Behavior Query Path)
+ * </p>
+ * @author DBFlute(AutoGenerator)
+ */
+public class BsWhiteCompoundPkWrongOrderBhvAp extends WhiteCompoundPkWrongOrderBhv {
+
+    // ===================================================================================
+    //                                                                          Definition
+    //                                                                          ==========
+    /*df:beginQueryPath*/
+    /*df:endQueryPath*/
+}
