ac3efbeb7954e219e9ce6bc7cd4cfd2188687f17
jflute <dbflute@gmail.com>
2014-11-25 17:04:37 +0900

    Release DBFlute-1.0.5N

--- dbflute-bhvap-seasar-example/src/main/java/com/example/dbflute/mysql/dbflute/bsentity/customize/BsAppPlainMember.java
+++ dbflute-bhvap-seasar-example/src/main/java/com/example/dbflute/mysql/dbflute/bsentity/customize/BsAppPlainMember.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 
 import org.seasar.dbflute.dbmeta.DBMeta;
 import org.seasar.dbflute.dbmeta.AbstractEntity;
+import org.seasar.dbflute.dbmeta.accessory.CustomizeEntity;
 import com.example.dbflute.mysql.dbflute.exentity.customize.*;
 
 /**
@@ -64,7 +65,7 @@ import com.example.dbflute.mysql.dbflute.exentity.customize.*;
  * </pre>
  * @author DBFlute(AutoGenerator)
  */
-public abstract class BsAppPlainMember extends AbstractEntity {
+public abstract class BsAppPlainMember extends AbstractEntity implements CustomizeEntity {
 
     // ===================================================================================
     //                                                                          Definition
